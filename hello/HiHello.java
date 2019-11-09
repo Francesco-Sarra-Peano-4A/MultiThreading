55243c7db7d10416734ad6e3bf2df388b14c28a7
Francesco-Sarra-Peano-4A <56753334+Francesco-Sarra-Peano-4A@users.noreply.github.com>
2019-11-09 10:15:52 +0100

    esercizio hi-hello
    in questo programma avviene stampa alternata di un secondo in maniera casuale , poichè si è usato due thread.

---
+++ hello/HiHello.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hi.hello;
+
+/**
+ *
+ * @author sarra.francesco
+ */
+public class HiHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Hello a = new Hello();
+        Hi b = new Hi();
+        a.start();
+        b.start();
+    }
+    
+}
